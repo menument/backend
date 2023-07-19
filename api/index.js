6b4cacbac2775530b7ef00752e2dc7fb4731376b
Bruno Vaz <brunobvaz@gmail.com>
2023-07-19 20:23:31 +0100

    first

--- api/index.js
+++ api/index.js
@@ -62,3 +62,4 @@ const PORT = process.env.PORT || 5000;
 // app.listen(PORT, () => console.log(`server running on port ${PORT}`));
 
 app.listen(PORT, () => console.log(`server running on port ${PORT}`));
+
